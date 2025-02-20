be34964181dc0e2c1ef870e9dbfe232ca84f7a2d
Darshan Shinde <darshan.leads09@gmail.com>
2025-02-20 19:53:50 +0530

    DB_CONFIG Changes

--- config.py
+++ config.py
@@ -1,16 +1,7 @@
-# DB_CONFIG = {
-#     'host': '216.48.185.197',
-#     'user': 'darsan',
-#     'password': 'Aloh@1234',
-#     'database': 'bhavcopynew',
-#     'port': 3306
-# }
-
-
 DB_CONFIG = {
     'host': '127.0.0.1',  # or 'localhost'
     'user': 'root',  # Replace with your local database username (e.g., 'root')
-    'password': 'Newday@123',  # Replace with your local database password
+    'password': '#########',  # Replace with your local database password
     'database': 'bhavcopynew',  # Replace with the name of the database you want to connect to
     'port': 3306  # Default MySQL port (if you're using a non-standard port, update accordingly)
 }
