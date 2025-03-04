1983576d982aa6a6c91ef815ec2b44360dca4a05
Darshan Shinde <darshan.leads09@gmail.com>
2025-03-04 11:24:21 +0530

    app2 prefix changes completed

--- bhavcopy_app/urls.py
+++ bhavcopy_app/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Add this line for the root URL
-    path('data/', views.get_data, name='get_data'),
-    path('reload/<str:date>/', views.reload_date, name='reload_date'),
-    path('mcx/', views.mcx_page, name='mcx_page'),
-    path('data/mcx/', views.get_data_mcx, name='get_data_mcx'),
-    path('reload/mcx/<str:date>/', views.reload_date_mcx, name='reload_date_mcx'),
+    path('app2/data/', views.get_data, name='get_data'),
+    path('app2/reload/<str:date>/', views.reload_date, name='reload_date'),
+    path('app2/mcx/', views.mcx_page, name='mcx_page'),
+    path('app2/data/mcx/', views.get_data_mcx, name='get_data_mcx'),
+    path('app2/reload/mcx/<str:date>/', views.reload_date_mcx, name='reload_date_mcx'),
 ]
 
